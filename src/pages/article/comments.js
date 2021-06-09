3d625f2add1235dd795300a9ee08b9194c2857bc
cvimard <cvimard@socialobjects.co>
2021-06-09 11:04:52 +0300

    feat(comments): create component class

--- src/pages/article/comments.js
+++ src/pages/article/comments.js
@@ -1,42 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Comments = () => (
-  <div className="mt-5">
-    <div className="row">
-      <div className="col-12">
-        <h4>Add Comment</h4>
-        <form>
-          <div className="mb-3">
-            <textarea className="form-control" id="sendcomment" rows="3" />
+const Comment = ({ item }) => {
+  const { date, text } = item;
+
+  return (
+    <li className="list-group-item">
+      <span>{date}</span>
+      {text}
+    </li>
+  );
+};
+
+class Comments extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      comments: []
+    };
+  }
+
+  render() {
+    const { comments } = this.state;
+
+    return (
+      <div className="mt-5">
+        <div className="row">
+          <div className="col-12">
+            <h4>Add Comment</h4>
+            <form>
+              <div className="mb-3">
+                <textarea className="form-control" id="sendcomment" rows="3" />
+              </div>
+              <div className="mb-3">
+                <button type="submit" className="btn btn-primary">Add Comment</button>
+              </div>
+            </form>
           </div>
-          <div className="mb-3">
-            <button type="submit" className="btn btn-primary">Add Comment</button>
+        </div>
+        <div className="row">
+          <div className="col-12">
+            <h4>View comments</h4>
           </div>
-        </form>
+          <div className="col-12">
+            <ul className="list-group">
+              {comments.map((item) => <Comment item={item} />)}
+            </ul>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="row">
-      <div className="col-12">
-        <h4>View comments</h4>
-      </div>
-      <div className="col-12">
-        <ul className="list-group">
-          <li className="list-group-item">
-            <span>09/06/2021</span>
-            A second item
-          </li>
-          <li className="list-group-item">
-            <span>09/06/2021</span>
-            A second item
-          </li>
-          <li className="list-group-item">
-            <span>09/06/2021</span>
-            A second item
-          </li>
-        </ul>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Comments;
